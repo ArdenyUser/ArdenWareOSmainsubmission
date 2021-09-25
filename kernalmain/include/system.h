41d27dc4186eb49aeed1e80c3f617d59b966f2dc
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-09-25 19:04:24 -0400

    Update system.h

--- kernalmain/include/system.h
+++ kernalmain/include/system.h
@@ -52,6 +52,29 @@ extern void timer_wait(int ticks);
 extern void timer_install();
 
 /* KEYBOARD.C */
-extern void keyboard_install();
+#include "x86.h"
+
+struct keyboard_flags {
+    unsigned char shift;
+    unsigned char ctrl;
+    unsigned char alt;
+    unsigned char caps_lock;
+    unsigned char num_lock;
+    unsigned char scroll_lock;
+};
+
+void keyboard_handler(struct regs* r);
+
+unsigned int keyboard_get_pointer();
+
+void keyboard_update_flags(unsigned char code);
+
+void keyboard_push(unsigned char data);
+
+unsigned char keyboard_pull();
+
+void keyboard_install();
+
+unsigned char keyboard_get_char(int keycode);
 
 #endif
