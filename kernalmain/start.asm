c849a8c87fedfdb953248eafe5b2480ff1c24f2f
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-09-25 14:44:45 -0400

    Update start.asm

--- kernalmain/start.asm
+++ kernalmain/start.asm
@@ -76,3 +76,11 @@ _gdt_flush:
 flush2:
     ret               ; Returns back to the C code!
 		
+; Loads the IDT defined in '_idtp' into the processor.
+; This is declared in C as 'extern void idt_load();'
+global _idt_load
+extern _idtp
+_idt_load:
+    lidt [_idtp]
+    ret
+		
