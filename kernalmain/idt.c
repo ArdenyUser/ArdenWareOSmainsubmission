b6e8cb1078076f67dcd715d29d1e2d6f3a4d004e
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-09-25 14:46:34 -0400

    Update idt.c

--- kernalmain/idt.c
+++ kernalmain/idt.c
@@ -1,12 +1,17 @@
-#include < system.h >
+/* bkerndev - Bran's Kernel Development Tutorial
+*  By:   Brandon F. (friesenb@gmail.com)
+*  Desc: Interrupt Descriptor Table management
+*
+*  Notes: No warranty expressed or implied. Use at own risk. */
+#include <system.h>
 
 /* Defines an IDT entry */
 struct idt_entry
 {
     unsigned short base_lo;
-    unsigned short sel;        /* Our kernel segment goes here! */
-    unsigned char always0;     /* This will ALWAYS be set to 0! */
-    unsigned char flags;       /* Set using the above table! */
+    unsigned short sel;
+    unsigned char always0;
+    unsigned char flags;
     unsigned short base_hi;
 } __attribute__((packed));
 
@@ -27,4 +32,36 @@ struct idt_ptr idtp;
 
 /* This exists in 'start.asm', and is used to load our IDT */
 extern void idt_load();
-		
+
+/* Use this function to set an entry in the IDT. Alot simpler
+*  than twiddling with the GDT ;) */
+void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags)
+{
+    /* The interrupt routine's base address */
+    idt[num].base_lo = (base & 0xFFFF);
+    idt[num].base_hi = (base >> 16) & 0xFFFF;
+
+    /* The segment or 'selector' that this IDT entry will use
+    *  is set here, along with any access flags */
+    idt[num].sel = sel;
+    idt[num].always0 = 0;
+    idt[num].flags = flags;
+}
+
+/* Installs the IDT */
+void idt_install()
+{
+    /* Sets the special IDT pointer up, just like in 'gdt.c' */
+    idtp.limit = (sizeof (struct idt_entry) * 256) - 1;
+    idtp.base = &idt;
+
+    /* Clear out the entire IDT, initializing it to zeros */
+    memset(&idt, 0, sizeof(struct idt_entry) * 256);
+
+    /* Add any new ISRs to the IDT here using idt_set_gate */
+
+
+
+    /* Points the processor's internal register to the new IDT */
+    idt_load();
+}
