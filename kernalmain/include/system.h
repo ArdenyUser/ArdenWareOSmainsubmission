1c3e5b0d6cdb8006943e66752d57ce87e85f4269
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-09-25 19:11:00 -0400

    Update system.h

--- kernalmain/include/system.h
+++ kernalmain/include/system.h
@@ -202,4 +202,11 @@ void irq_remap(void);
 
 void irq_install();
 
+struct regs {
+    unsigned int gs, fs, es, ds;
+    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    unsigned int int_no, err_code;
+    unsigned int eip, cs, eflags, useresp, ss;
+};
+
 #endif
