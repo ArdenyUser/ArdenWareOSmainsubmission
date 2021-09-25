d3a00e86af4c74532969c4ae3ff7258a61ec5e73
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-09-25 16:18:53 -0400

    Update system.h

--- kernalmain/include/system.h
+++ kernalmain/include/system.h
@@ -1,33 +1,57 @@
+/* bkerndev - Bran's Kernel Development Tutorial
+*  By:   Brandon F. (friesenb@gmail.com)
+*  Desc: Global function declarations and type definitions
+*
+*  Notes: No warranty expressed or implied. Use at own risk. */
 #ifndef __SYSTEM_H
 #define __SYSTEM_H
 
-/* MAIN.C */
-extern unsigned char *memcpy(unsigned char *dest, const unsigned char *src, int count);
-extern unsigned char *memset(unsigned char *dest, unsigned char val, int count);
-extern unsigned short *memsetw(unsigned short *dest, unsigned short val, int count);
-extern int strlen(const char *str);
-extern unsigned char inportb (unsigned short _port);
-extern void outportb (unsigned short _port, unsigned char _data);
-
-extern void cls();
-extern void putch(unsigned char c);
-extern void puts(unsigned char *str);
-extern void settextcolor(unsigned char forecolor, unsigned char backcolor);
-extern void init_video();
-extern void gdt_set_gate();
-extern void gdt_install();
-extern void idt_set_gate();
-extern void idt_install();
+typedef int size_t;
 
 /* This defines what the stack looks like after an ISR was running */
 struct regs
 {
-    unsigned int gs, fs, es, ds;      /* pushed the segs last */
-    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;  /* pushed by 'pusha' */
-    unsigned int int_no, err_code;    /* our 'push byte #' and ecodes do this */
-    unsigned int eip, cs, eflags, useresp, ss;   /* pushed by the processor automatically */ 
+    unsigned int gs, fs, es, ds;
+    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    unsigned int int_no, err_code;
+    unsigned int eip, cs, eflags, useresp, ss;    
 };
-		
 
+/* MAIN.C */
+extern void *memcpy(void *dest, const void *src, size_t count);
+extern void *memset(void *dest, char val, size_t count);
+extern unsigned short *memsetw(unsigned short *dest, unsigned short val, size_t count);
+extern size_t strlen(const char *str);
+extern unsigned char inportb (unsigned short _port);
+extern void outportb (unsigned short _port, unsigned char _data);
+
+/* CONSOLE.C */
+extern void init_video(void);
+extern void puts(unsigned char *text);
+extern void putch(unsigned char c);
+extern void cls();
+
+/* GDT.C */
+extern void gdt_set_gate(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran);
+extern void gdt_install();
+
+/* IDT.C */
+extern void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags);
+extern void idt_install();
+
+/* ISRS.C */
+extern void isrs_install();
+
+/* IRQ.C */
+extern void irq_install_handler(int irq, void (*handler)(struct regs *r));
+extern void irq_uninstall_handler(int irq);
+extern void irq_install();
+
+/* TIMER.C */
+extern void timer_wait(int ticks);
+extern void timer_install();
+
+/* KEYBOARD.C */
+extern void keyboard_install();
 
 #endif
