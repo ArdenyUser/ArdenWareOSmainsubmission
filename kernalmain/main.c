d52a11f79cff1fabae247d5dd7ba324da21e4483
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-09-25 14:53:31 -0400

    Update main.c

--- kernalmain/main.c
+++ kernalmain/main.c
@@ -1,38 +1,39 @@
+/* bkerndev - Bran's Kernel Development Tutorial
+*  By:   Brandon F. (friesenb@gmail.com)
+*  Desc: Main.c: C code entry.
+*
+*  Notes: No warranty expressed or implied. Use at own risk. */
+#include <system.h>
 
-gdt_install();
-#include < system.h >
-
-/* You will need to code these up yourself!  */
-unsigned char *memcpy(unsigned char *dest, const unsigned char *src, int count)
+void *memcpy(void *dest, const void *src, size_t count)
 {
-    /* Add code here to copy 'count' bytes of data from 'src' to
-    *  'dest', finally return 'dest' */
+    const char *sp = (const char *)src;
+    char *dp = (char *)dest;
+    for(; count != 0; count--) *dp++ = *sp++;
+    return dest;
 }
 
-unsigned char *memset(unsigned char *dest, unsigned char val, int count)
+void *memset(void *dest, char val, size_t count)
 {
-    /* Add code here to set 'count' bytes in 'dest' to 'val'.
-    *  Again, return 'dest' */
+    char *temp = (char *)dest;
+    for( ; count != 0; count--) *temp++ = val;
+    return dest;
 }
 
-unsigned short *memsetw(unsigned short *dest, unsigned short val, int count)
+unsigned short *memsetw(unsigned short *dest, unsigned short val, size_t count)
 {
-    /* Same as above, but this time, we're working with a 16-bit
-    *  'val' and dest pointer. Your code can be an exact copy of
-    *  the above, provided that your local variables if any, are
-    *  unsigned short */
+    unsigned short *temp = (unsigned short *)dest;
+    for( ; count != 0; count--) *temp++ = val;
+    return dest;
 }
 
-int strlen(const char *str)
+size_t strlen(const char *str)
 {
-    /* This loops through character array 'str', returning how
-    *  many characters it needs to check before it finds a 0.
-    *  In simple words, it returns the length in bytes of a string */
+    size_t retval;
+    for(retval = 0; *str != '\0'; str++) retval++;
+    return retval;
 }
 
-/* We will use this later on for reading from the I/O ports to get data
-*  from devices such as the keyboard. We are using what is called
-*  'inline assembly' in these routines to actually do the work */
 unsigned char inportb (unsigned short _port)
 {
     unsigned char rv;
@@ -40,23 +41,29 @@ unsigned char inportb (unsigned short _port)
     return rv;
 }
 
-/* We will use this to write to I/O ports to send bytes to devices. This
-*  will be used in the next tutorial for changing the textmode cursor
-*  position. Again, we use some inline assembly for the stuff that simply
-*  cannot be done in C */
 void outportb (unsigned short _port, unsigned char _data)
 {
     __asm__ __volatile__ ("outb %1, %0" : : "dN" (_port), "a" (_data));
 }
 
-/* This is a very simple main() function. All it does is sit in an
-*  infinite loop. This will be like our 'idle' loop */
 void main()
 {
-    /* You would add commands after here */
+    int i;
 
-    /* ...and leave this loop in. There is an endless loop in
-    *  'start.asm' also, if you accidentally delete this next line */
+    gdt_install();
+    idt_install();
+    isrs_install();
+    irq_install();
+    init_video();
+    timer_install();
+    keyboard_install();
+
+    __asm__ __volatile__ ("sti");
+
+    puts("Hello World!\n");
+
+//    i = 10 / 0;
+//    putch(i);
+
     for (;;);
 }
-		
