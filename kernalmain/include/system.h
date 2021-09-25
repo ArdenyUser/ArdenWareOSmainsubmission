0acbcc749830ee79425b407657b7c1457bcbc9af
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-09-25 14:59:26 -0400

    Update system.h

--- kernalmain/include/system.h
+++ kernalmain/include/system.h
@@ -19,4 +19,15 @@ extern void gdt_install();
 extern void idt_set_gate();
 extern void idt_install();
 
+/* This defines what the stack looks like after an ISR was running */
+struct regs
+{
+    unsigned int gs, fs, es, ds;      /* pushed the segs last */
+    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;  /* pushed by 'pusha' */
+    unsigned int int_no, err_code;    /* our 'push byte #' and ecodes do this */
+    unsigned int eip, cs, eflags, useresp, ss;   /* pushed by the processor automatically */ 
+};
+		
+
+
 #endif
