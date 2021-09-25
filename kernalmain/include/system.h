50d7a5903d2f584c05590b0b69005423d248d1b0
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-09-25 14:39:11 -0400

    Update system.h

--- kernalmain/include/system.h
+++ kernalmain/include/system.h
@@ -14,5 +14,6 @@ extern void putch(unsigned char c);
 extern void puts(unsigned char *str);
 extern void settextcolor(unsigned char forecolor, unsigned char backcolor);
 extern void init_video();
+extern void gdt_install();
 
 #endif
