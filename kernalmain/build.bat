099458658a29726de141c7e676aa67f892900b9b
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-09-25 14:30:58 -0400

    Add files via upload

---
+++ kernalmain/build.bat
@@ -0,0 +1,11 @@
+echo Now assembling, compiling, and linking your kernel:
+nasm -f aout -o start.o start.asm
+rem Remember this spot here: We will add 'gcc' commands here to compile C sources
+
+
+rem This links all your files. Remember that as you add *.o files, you need to
+rem add them after start.o. If you don't add them at all, they won't be in your kernel!
+ld -T link.ld -o kernel.bin start.o
+echo Done!
+pause
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o main.o main.c
