fa4cadd392c45d48b141f1dede2a2b5b8b8ea1ba
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-09-25 15:17:23 -0400

    Update timer.c

--- kernalmain/timer.c
+++ kernalmain/timer.c
@@ -29,4 +29,14 @@ void timer_install()
     /* Installs 'timer_handler' to IRQ0 */
     irq_install_handler(0, timer_handler);
 }
+
+/* This will continuously loop until the given time has
+*  been reached */
+void timer_wait(int ticks)
+{
+    unsigned long eticks;
+
+    eticks = timer_ticks + ticks;
+    while(timer_ticks < eticks);
+}
 	
