f15785c818d48b3a605a86559001be02370eaaf6
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-09-27 20:08:17 -0400

    Update build.sh

--- kernalmain/build.sh
+++ kernalmain/build.sh
@@ -9,7 +9,6 @@ nasm -f aout -o start.o start.asm
 
 gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o main.o main.c
 
-gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o scrn.o scrn.c
 
 gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o gdt.o gdt.c
 
@@ -23,7 +22,7 @@ gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc
 
 gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o keyboard.o keyboard.c
 
-gcc  -g test1.c path.c inode.c super.c cache.c filedes.c myfs.c ./socket/client.c -o filesystem.o
 
-ld -T linker.ld -o kernel.bin start.o main.o scrn.o gdt.o idt.o isrs.o irq.o timer.o keyboard.o filesystem.o
+
+ld -T linker.ld -o kernel.bin start.o main.o scrn.o gdt.o idt.o isrs.o irq.o timer.o keyboard.o 
 
